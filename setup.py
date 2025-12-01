a4164448ffaac93b249fb036a2477d41833577f3
Andrea Gavazzi <andrea.gavazzi@me.com>
2025-12-01 20:26:55 +0100

    bugfixses

--- setup.py
+++ setup.py
@@ -8,15 +8,23 @@ setup(
     name=package_name,
     version='0.0.0',
     packages=[package_name],
+
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
         (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
-        (os.path.join('share', package_name, 'meshes'), glob('meshes/*')),
+
+        # Meshes: separo visual e collision
+        (os.path.join('share', package_name, 'meshes', 'visual'),
+            glob('meshes/visual/*')),
+        (os.path.join('share', package_name, 'meshes', 'collision'),
+            glob('meshes/collision/*')),
+
         (os.path.join('share', package_name, 'config'), glob('config/*'))
     ],
+
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='author',
