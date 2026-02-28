7d199d80d241ced723dab5053818a52e9cf8d7f3
Andrea Gavazzi <andrea.gavazzi@me.com>
2026-02-28 23:57:06 +0100

    update

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     ],
 
     install_requires=['setuptools'],
-    zip_safe=True,
+    zip_safe=False,
     maintainer='author',
     maintainer_email='todo@todo.com',
     description='The ' + package_name + ' package',
